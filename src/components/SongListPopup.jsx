bc44d3c9a188858bf9740b8a42bf1d1d3a320630
saikatsom234@gmail.com <293373936+saikatsom234@users.noreply.github.com>
2026-09-02 18:21:26 +0530

    Update 9th song to Dosh Karo Noy Go Ma

--- src/components/SongListPopup.jsx
+++ src/components/SongListPopup.jsx
@@ -171,7 +171,7 @@ const SongListPopup = ({ onClose, onSelectSong, currentSong }) => {
     { id: 15, title: 'Ogo Amar Agamani', artist: 'Sipra Bose', duration: '3:41', cover: '/mahalaya-cover.jpg', src: '/songs/ogo-amar-agamani-alo.mp3' },
     { id: 16, title: 'Tabo Achinta Rupa', artist: 'Manabendra Mukherjee', duration: '4:25', cover: '/mahalaya-cover.jpg', src: '/songs/tabo-achinta-rupa.mp3' },
     { id: 17, title: 'Jago Durga Dashapraharanadharinee', artist: 'Dwijen Mukherjee', duration: '1:45', cover: '/mahalaya-cover.jpg', src: '/songs/jago-durga.mp3' },
-    { id: 18, title: 'Bimano Bihari', artist: 'Tarun Banerjee', duration: '5:05', cover: '/mahalaya-cover.jpg', src: '/songs/dugga-elo.mp3' },
+    { id: 18, title: 'Dosh Karo Noy Go Ma', artist: 'Pannalal Bhattacharya', duration: '3:26', cover: '/mahalaya-cover.jpg', src: '/songs/dosh-karo-noy-go-ma.mp3' },
     { id: 19, title: 'Amal Kiran', artist: 'Sandhya Mukherjee', duration: '3:50', cover: '/mahalaya-cover.jpg', src: '/songs/dugga-elo.mp3' },
     { id: 20, title: 'Jaya Jaya Japya', artist: 'Chorus', duration: '4:15', cover: '/mahalaya-cover.jpg', src: '/songs/dugga-elo.mp3' },
   ];
