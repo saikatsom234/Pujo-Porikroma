29aa899ce97b26303bd692d5560895ee631ca7f1
saikatsom234@gmail.com <293373936+saikatsom234@users.noreply.github.com>
2026-09-06 07:39:29 +0530

    Update MapPage TileLayer to Stamen Toner Lite for monochrome aesthetic

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -111,9 +111,9 @@ const MapPage = ({ onClose }) => {
           style={{ width: '100%', height: '100%' }}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
-            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-            maxZoom={19}
+            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, under <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a>. Data by <a href="http://openstreetmap.org">OpenStreetMap</a>, under <a href="http://creativecommons.org/licenses/by-sa/3.0">CC BY-SA</a>.'
+            url="https://stamen-tiles.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png"
+            maxZoom={20}
             subdomains="abcd"
           />
           <ZoomControl position="bottomright" />
