a3ddb6ba7830da98cf1d0b024157518a108a29b2
saikatsom234@gmail.com <293373936+saikatsom234@users.noreply.github.com>
2026-09-07 08:27:19 +0530

    Fix 401 map authentication error by swapping Stadia tiles out for CartoDB Positron which provides the same light smooth theme but requires no authentication on external domains

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -595,8 +595,8 @@ const MapPage = ({ onClose }) => {
           style={{ width: '100%', height: '100%' }}
         >
           <TileLayer
-            attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-            url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
             maxZoom={20}
           />
           
