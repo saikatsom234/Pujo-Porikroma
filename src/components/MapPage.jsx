7ac624f43294a3845702f16892708e6055718ed3
saikatsom234@gmail.com <293373936+saikatsom234@users.noreply.github.com>
2026-09-07 11:02:45 +0530

    Change map tile provider to CartoDB Positron with provided API key to remove watermark and restore clean light theme

--- src/components/MapPage.jsx
+++ src/components/MapPage.jsx
@@ -599,9 +599,9 @@ const MapPage = ({ onClose }) => {
           style={{ width: '100%', height: '100%' }}
         >
           <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
-            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-            maxZoom={19}
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
+            url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png?key=cb1_2zpy_1_5578b74846ef709b32860fd7"
+            maxZoom={20}
           />
           
           <RecenterMap center={mapCenter} zoom={mapZoom} />
